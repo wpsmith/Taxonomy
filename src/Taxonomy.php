7f475018b99a3052d8e053c23f4bdff5dc32f015
wpsmith <t@wpsmith.net>
2018-06-30 13:32:23 -0400

    Add add_action method

--- src/Taxonomy.php
+++ src/Taxonomy.php
@@ -500,5 +500,32 @@ if ( ! class_exists( 'WPS\Taxonomies\Taxonomy' ) ) {
 			return $sorted;
 		}
 
+		/**
+		 * Hooks a function on to a specific action.
+		 *
+		 * Actions are the hooks that the WordPress core launches at specific points
+		 * during execution, or when specific events occur. Plugins can specify that
+		 * one or more of its PHP functions are executed at these points, using the
+		 * Action API.
+		 *
+		 * @since 1.2.0
+		 *
+		 * @param string   $tag             The name of the action to which the $function_to_add is hooked.
+		 * @param callable $function_to_add The name of the function you wish to be called.
+		 * @param int      $priority        Optional. Used to specify the order in which the functions
+		 *                                  associated with a particular action are executed. Default 10.
+		 *                                  Lower numbers correspond with earlier execution,
+		 *                                  and functions with the same priority are executed
+		 *                                  in the order in which they were added to the action.
+		 * @param int      $accepted_args   Optional. The number of arguments the function accepts. Default 1.
+		 */
+		public function add_action( $tag, $function_to_add, $priority = 10, $accepted_args = 1 ) {
+			if ( did_action( $tag ) || doing_action( $tag ) ) {
+				call_user_func_array( $function_to_add, array() );
+			} else {
+				add_action( $tag, $function_to_add, $priority, $accepted_args );
+			}
+		}
+
 	}
 }
